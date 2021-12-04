7bc2ba5cc29fa2c6d9bd44dd5cac84ccd18476aa
Vudang <danglegiavu@gmail.com>
2021-12-04 18:52:21 +0700

    add category module

---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Hotel } from "../../hotel/entities/hotel.entity";
+
+
+@Entity('categories')
+export class Category {
+    @PrimaryGeneratedColumn()
+    id : number;
+
+    @ApiProperty()
+    @Column({
+        unique:false ,
+        name:'category_name' 
+    })
+    categoryName : string
+
+    @ApiProperty()
+    @OneToMany(() => Hotel,hotel => hotel.category,{
+        cascade:['remove','update'],
+        eager:true
+    }) 
+    hotels: Hotel[]
+
+}
