f1c5615b0c0fa1b0e6e5bcd60a31aafa019b6caf
Vudang <danglegiavu@gmail.com>
2021-12-04 18:52:43 +0700

    add country module

---
+++ src/country/entities/country.entity.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { City } from "../../city/entities/city.entity";
+
+@Entity('countries')
+export class Country {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ApiProperty()
+    @Column({
+        name:'country_name',
+        nullable:false
+    })
+    countryName: string
+
+    @OneToMany(() => City,city => city.country,{
+        lazy:true,
+        cascade:['remove','update']
+    })
+    cities: City[]
+
+
+
+}
